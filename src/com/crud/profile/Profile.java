dca5a1b20679506fcf4efeef7fab84c019c85a70
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-04-08 19:13:25 -0300

    Profile class -> Getter and Setter lombok added

--- src/com/crud/profile/Profile.java
+++ src/com/crud/profile/Profile.java
@@ -1,6 +1,11 @@
 package com.crud.profile;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Profile {
-    String name;
-    int age;
+    private String name;
+    private int age;
 }
