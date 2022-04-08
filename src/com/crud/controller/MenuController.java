831b53d50de87bde8b561429d34af65bffb0a9a2
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-04-08 19:47:26 -0300

    MenuController -> read method is now getting a given profile name and showing that information to the user

--- src/com/crud/controller/MenuController.java
+++ src/com/crud/controller/MenuController.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 import com.crud.profile.Profile;
 
+
 public class MenuController {
     
     HashMap<String, Profile> profileList = new HashMap<>();
@@ -54,10 +55,30 @@ public class MenuController {
         newProfile.setAge(sc.nextInt());
 
         profileList.put(newProfile.getName(), newProfile);
+
+        System.out.println("Profile creation completed");
+
+        chooseOption();
     }
 
     void read() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please, enter profile name -> ");
         
+        String name = sc.nextLine();
+
+        if(profileList.containsKey(name)) {
+            System.out.println("Name -> " + profileList.get(name).getName());
+            System.out.println("Age -> "+ profileList.get(name).getAge());
+        }
+
+        else {
+            System.out.printf("\nERROR -> There's no %s recorded\n", name);
+            chooseOption();
+        }
+
+
     }
 
     void update() {
